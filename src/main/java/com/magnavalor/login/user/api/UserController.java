753ad787f80cc8177fb9e94c4d836f49c99711b5
ramazan-aktas <ramazanaktas@protonmail.com>
2022-08-27 18:08:57 +0300

    git commit -m "controller and service updates"

--- src/main/java/com/magnavalor/login/user/api/UserController.java
+++ src/main/java/com/magnavalor/login/user/api/UserController.java
@@ -5,10 +5,9 @@ import com.magnavalor.login.user.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -21,5 +20,19 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user){
         return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+    }
+    @PatchMapping
+    public ResponseEntity<Optional<User>> updateUser(@RequestBody User user){
+        return new ResponseEntity<>(userService.updateUser(user), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Optional<User>> getUser(@RequestBody User user){
+        return new ResponseEntity<>(userService.getUser(user), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteUser(@RequestBody User user){
+        return new ResponseEntity<>(userService.deleteUser(user.getId()), HttpStatus.ACCEPTED);
     }
 }
